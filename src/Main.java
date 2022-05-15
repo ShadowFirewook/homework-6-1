900335c938e61960eaf7b2e2b7a53f9a7af381f4
ShadowFirewook <104684219+ShadowFirewook@users.noreply.github.com>
2022-05-15 19:49:15 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Boss boss = new Boss(1000,65);
-        System.out.println("Здоровье: " + boss.getHP() + " Урон: "+ boss.getDMG());
+        Weapon weapon = new Weapon("Steel Arms", "Sword");
+        Boss boss = new Boss(1000,65 );
+        boss.setWeapon(weapon);
+        System.out.println("Здоровье: " + boss.getHP() + " Урон: "+ boss.getDMG() + " Тип оружия: " + boss.getWeapon().getWeaponType() +" Оружие: "+ boss.getWeapon().getWeaponName());
     }
 }
