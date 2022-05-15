d7e8455a779f47d9bf1aecc26b2e1cd1996a50ed
ShadowFirewook <104684219+ShadowFirewook@users.noreply.github.com>
2022-05-15 19:49:44 +0300

    Update Weapon.java

--- src/Weapon.java
+++ src/Weapon.java
@@ -2,4 +2,24 @@ public class Weapon {
     private String weaponType;
     private String weaponName;
 
+    public Weapon(String weaponType,String weaponName){
+    this.weaponType = weaponType;
+    this.weaponName = weaponName;
+    }
+
+    public String getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(String weaponType) {
+        this.weaponType = weaponType;
+    }
+
+    public String getWeaponName() {
+        return weaponName;
+    }
+
+    public void setWeaponName(String weaponName) {
+        this.weaponName = weaponName;
+    }
 }
